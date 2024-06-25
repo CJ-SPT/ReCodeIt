b6c196f8277eb9c95d0856ef970cb1f131cdec25
Cj <161484149+CJ-SPT@users.noreply.github.com>
2024-06-25 08:51:57 -0400

    fix logger not resetting color

--- RecodeItLib/Utils/Logger.cs
+++ RecodeItLib/Utils/Logger.cs
@@ -29,6 +29,7 @@ public static class Logger
         if (!silent)
         {
             Console.ForegroundColor = color;
+            Console.ResetColor();
             Console.WriteLine(message);
         }
 
